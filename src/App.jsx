4481ed9f059b73a9291b3f59a8a93edb7a388c84
Brian <bnmmendoza.student@ua.edu.ph>
2025-09-19 23:04:47 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+
+function HeaderContainer() {
+  return <div className="header-container"></div>;
+}
+
+function HeaderContainer2() {
+  return <div className="header-container2">
+    <HeaderContainer2Content name="Brian"/>
+    <HeaderContainer2Content name="Mendoza"/>
+  </div>;
+}
+
+function HeaderContainer2Content(props) {
+  return <div className="header-container2-content">{props.name}</div>;
+}
+
+function Header() {
+  return <div className="header">
+    <HeaderContainer />
+    <HeaderContainer2 />
+    <HeaderContainer />
+  </div>;
+}
+
+function BodyContainer() {
+  return <div className="body-container"></div>;
+}
+
+function BodyContainer2Content() {
+  return <div className="body-container2-content"></div>;
+}
+
+function BodyContainer2() {
+  return <div className="body-container2">
+    <BodyContainer2Content />
+    <BodyContainer2Content />
+  </div>;
+}
+
+function Body() {
+  return <div className="body">
+    <BodyContainer />
+    <BodyContainer2 />
+  </div>;
+}
+
+function FooterContainer1() {
+  return <div className="footer-container1"></div>;
+}
+function FooterContainer2(props) {
+  return <div className="footer-container2">{props.text}</div>;
+}
+
+function Footer() {
+  return <div className="footer">
+    <FooterContainer1 />
+    <FooterContainer2 text="C-PCIT9"/>
+    <FooterContainer2 text="IT3B"/>
+    <FooterContainer1 />
+  </div>;
+}
+
+function App() {
+  return (
+    <div className="container">
+      <Header name="Brian"/>
+      <Body />
+      <Footer name="Mendoza"/>
+    </div>
+  )
+}
+
+export default App
